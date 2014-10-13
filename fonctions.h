8f1c2b3f8618f88eebc63e849fc36845c8d0d504
samtruelle <s.reymondet@gmail.com>
2014-10-13 18:19:37 +0200

    fonctions.h
    lexique des fonctions (non commenté)

---
+++ fonctions.h
@@ -0,0 +1,24 @@
+#ifndef FONCTIONS_H
+#define FOCNTIONS_H
+
+#include "structures.h"
+
+t_ludotheque* creer_ludotheque();
+
+t_jeu* creer_jeu(char * nom, int nbJoueurMin, int nbJoueurMax, int duree,genre_jeu genre);
+
+int ajouter_jeu(t_ludotheque * ludo,t_jeu * j);
+
+void afficher_ludotheque(t_ludotheque* ludo);
+
+int retirer_jeu(t_ludotheque* ludo, char* nom);
+
+void supprimer_ludotheque(t_ludotheque* ludo);
+
+t_ludotheque* requete_jeu(t_ludotheque* ludo, genre_jeu genre, int nbJoueurs, int duree);
+
+t_ludotheque* fusion(t_ludotheque* ludo1, t_ludotheque* ludo2);
+
+
+
+#endif
